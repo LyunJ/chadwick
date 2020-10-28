daacb86b365c547884c523143bce32efefdce758
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-10-28 21:58:45 +0900

    change database

--- pdos/DatabasePdo.php
+++ pdos/DatabasePdo.php
@@ -4,10 +4,10 @@
 function pdoSqlConnect()
 {
     try {
-        $DB_HOST = "tastereview.cevu32mso1il.ap-northeast-2.rds.amazonaws.com";
+        $DB_HOST = "chadwicktastereview.cevu32mso1il.ap-northeast-2.rds.amazonaws.com";
         $DB_NAME = "TasteReview";
-        $DB_USER = "lyunj";
-        $DB_PW = "chadwik2020!";
+        $DB_USER = "root";
+        $DB_PW = "chadwick2020!";
         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
