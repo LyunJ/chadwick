e40fb0e8e454b032d672b15c463483dd3993fd58
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-10-29 22:33:30 +0900

    date format apply

--- pdos/MenuPdo.php
+++ pdos/MenuPdo.php
@@ -3,6 +3,8 @@
 function addMenu($date,$foodType,$menuName){
     $pdo = pdoSqlConnect();
     $query = "insert into menu (foodCategoryIdx,date,menuName) values (?,?,?)";
+
+    $date = date('Y-m-d',strtotime($date));
     foreach ($menuName as $menu){
         $st = $pdo -> prepare($query);
         $st -> execute([$foodType,$date,$menu]);
